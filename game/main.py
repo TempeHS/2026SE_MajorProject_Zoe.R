fbce1c0a081ad217b584cb27a1d600b156f5c093
Zoë R <126206627+ThatOneArtKid@users.noreply.github.com>
2026-06-03 12:51:28 +1000

    basic setup stuff

--- game/main.py
+++ game/main.py
@@ -5,8 +5,9 @@ import time
 #initialises pygame
 pg.init()
 screen = pg.display.set_mode((640,640))
-smiley = pg.image.load('game/you-are-an-idiot.png').convert_alpha()
+smiley = pg.image.load('you-are-an-idiot.png').convert_alpha()
 clock = pg.time.Clock()
+font = pg.font.Font(None, size=30)
 
 running = True
 x=-500
@@ -14,10 +15,13 @@ while running:
     #sets the screen colour
     screen.fill((100,100,100))
     #caps the framerate at 60fps
-    clock.tick(20)
+    clock.tick(60)
     #sets smiley position
     screen.blit(smiley, (x, 0))
     x+=1
+    #render text
+    text = font.render("test game :3", True, (0,0,0))
+    screen.blit(text,(250,25))
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
