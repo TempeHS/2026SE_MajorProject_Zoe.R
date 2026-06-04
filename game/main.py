05263bdc8d019735502b476fd78edef621211e69
Zoë R <126206627+ThatOneArtKid@users.noreply.github.com>
2026-06-04 10:36:33 +1000

    Basic setup finished
    Has file for testing some basic pygame features (mouse clicks, collisions, displaying objects, etc), as well as web support

--- game/main.py
+++ game/main.py
@@ -18,12 +18,44 @@ while running:
     clock.tick(60)
     #sets smiley position
     screen.blit(smiley, (x, 0))
+    #get mouse position
+    mousepos = pg.mouse.get_pos()
+    #checks to see if left click has been pressed
+    mclick = pg.mouse.get_pressed()[0]
+    #basic rectangle to detect for collisions
+    target = pg.Rect(300,0,160,280)
+    #detects mouse collisions
+    collision = target.collidepoint(mousepos)
+    mcollision = False
+    if collision == True and mclick == True:
+        mcollision = True
+    #draws rectangle
+    pg.draw.rect(screen,(255*mcollision,255,0),target)
     x+=1
     #render text
-    text = font.render("test game :3", True, (0,0,0))
+    text = font.render("test game", True, (0,0,0))
+    text2 = font.render("(it's a little ugly but that's ok :3)", True, (0,0,0))
     screen.blit(text,(250,25))
+    screen.blit(text2,(150,50))
+    #game quitting event 
+    #(prevents the game from freezing)
     for event in pg.event.get():
+        #checks if the mouse is being clicked over a specific object
+        #good basic code but would like to check if I can do this using variables 
+        #as it'll probably be more efficient
+        # if event.type == pg.MOUSEBUTTONDOWN:
+        #     if collision == True:
+        #         print("click")
         if event.type == pg.QUIT:
             running = False
+    #updates display
     pg.display.flip()
 pg.quit()
+
+#TEST GAME FUNCTIONS
+#displays object
+#sets screen colour
+#moves object across screen
+#detects collisions (specifically for the mouse but this can be modified for other objects too)
+#detects clicks on certain objects (in TWO different ways :0)
+#changes rectangle colours
