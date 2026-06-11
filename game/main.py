8257606c98bbadc1c40724cd004e2d34ee941b12
Zoë R <126206627+ThatOneArtKid@users.noreply.github.com>
2026-06-11 10:35:17 +1000

    wip: basic dialogue system setup

--- game/main.py
+++ game/main.py
@@ -12,9 +12,20 @@ font = pg.font.Font(None, size=30)
 running = True
 x=-500
 amount_clicks = 0
+data = []
+with open("dialogue_placeholder") as file:
+    for line in file:
+        row = line.rstrip()
+        data.append(row)
+file.close()
+
 while running:
     #sets the screen colour
-    screen.fill((100,100,100))
+    screen.fill((50,100,150))
+    text = font.render("test game", True, (0,0,0))
+    text2 = font.render("(it's a little ugly but that's ok :3)", True, (0,0,0))
+    screen.blit(text,(250,25))
+    screen.blit(text2,(150,50))
     #caps the framerate at 60fps
     clock.tick(60)
     #sets smiley position
@@ -24,31 +35,29 @@ while running:
     #checks to see if left click has been pressed
     # mclick = pg.mouse.get_pressed()[0]
     #basic rectangle to detect for collisions
-    target = pg.Rect(300,0,160,280)
+    target = pg.Rect(225,150,200,200)
     #detects mouse collisions
     collision = target.collidepoint(mousepos)
-    mcollision = False
+    # mcollision = False
     # if collision == True and mclick == True:
     #     mcollision = True
     #     amount_clicks += 1
     #     print(f"clicked {amount_clicks} times")
     #draws rectangle
-    pg.draw.rect(screen,(255*mcollision,255,0),target)
+    pg.draw.rect(screen,(255,255,0),target)
     # x+=1
     #render text
-    text = font.render("test game", True, (0,0,0))
-    text2 = font.render("(it's a little ugly but that's ok :3)", True, (0,0,0))
-    screen.blit(text,(250,25))
-    screen.blit(text2,(150,50))
     #game quitting event 
     #(prevents the game from freezing)
     for event in pg.event.get():
         #checks if the mouse is being clicked over a specific object
         if event.type == pg.MOUSEBUTTONDOWN:
             if collision == True:
+                if amount_clicks < len(data) - 1:
+                    print(data[amount_clicks])
+                else:
+                    print(data[len(data) - 1])
                 amount_clicks +=1
-                print(f"clicked {amount_clicks} times")
-                mcollision = True
         if event.type == pg.QUIT:
             running = False
     #updates display
@@ -62,3 +71,7 @@ pg.quit()
 #detects collisions (specifically for the mouse but this can be modified for other objects too)
 #detects clicks on certain objects (in TWO different ways :0)
 #counts the amount of mouse clicks on objects
+#basic dialogue system
+
+#basic dialogue system works for testing, but will probably need to be optimised a bit to make it work for multiple objects
+#(probably by turning it into a function)
